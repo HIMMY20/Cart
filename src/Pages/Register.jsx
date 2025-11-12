81b3b3fdfaaa0acd363e9d1aa946a26c1ad29f61
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2025-11-12 14:42:07 +0530

    first commit

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -16,8 +16,8 @@ const Register = () => {
   const handlesubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/signup",form);
-      // const res = await axios.post("https://your-backend.onrender.com/register",form);
+      // const res = await axios.post("http://localhost:5000/signup",form);
+      const res = await axios.post("https://cart-backend-ten.vercel.app/register",form);
        alert(res.data.message);
       
     } catch (error) {
