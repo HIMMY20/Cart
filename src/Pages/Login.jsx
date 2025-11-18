f965a45bffa583f067a093a4d773051a89f031ac
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2025-11-18 17:06:33 +0530

    Vercel backend setup

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,7 +18,7 @@
         e.preventDefault();
         try {
         // const res = await axios.post("http://localhost:5000/login",form);
-        await axios.post("https://backend-cart-v1o5.vercel.app/login", form);
+        const res = await axios.post("https://backend-cart-v1o5.vercel.app/login", form);
 
         if(res.data.success){
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     e.preventDefault();
     try {
       // const res = await axios.post("http://localhost:5000/signup",form);
-      await axios.post("https://backend-cart-v1o5.vercel.app/signup", form);
+     const res =  await axios.post("https://backend-cart-v1o5.vercel.app/signup", form);
        alert(res.data.message);
       
     } catch (error) {
